b946b76a3e58e698f8d1e3595040a27910104d5b
rezhaandri22 <rezha1998@gmail.com>
2024-11-26 09:01:54 +0700

    Update

--- cypress/e2e/OrangeHRM/quiz3.cy.js
+++ cypress/e2e/OrangeHRM/quiz3.cy.js
@@ -8,10 +8,25 @@ describe("Login Tests - OrangeHRM", () => {
   });
 
   it("TC-001: User dapat login menggunakan akun data valid", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
     cy.get("h5").should("contain.text", "Login");
     cy.get('[name="username"]').type("Admin");
     cy.get('[name="password"]').type("admin123");
+    cy.intercept("GET", "**/messages").as("messages");
+    cy.intercept("GET", "**/action-summary").as("actionSummary");
+    cy.intercept("GET", "**/shortcuts").as("shortcuts");
+    cy.intercept("GET", "**/subunit").as("subunit");
+    cy.intercept("POST", "**/push").as("push");
+    cy.intercept("GET", "**/locations").as("locations");
     cy.get('[type="submit"]').click();
+    cy.wait("@messages");
+    cy.wait("@actionSummary");
+    cy.wait("@shortcuts");
+    cy.wait("@subunit");
+    cy.wait("@push");
+    cy.wait("@locations");
     cy.get("h6").should("contain.text", "Dashboard");
   });
 
